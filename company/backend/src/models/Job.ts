8b9cd1a3ef20526d175dc9db2daadb1edced3ae9
Chethan <gchethan7602@gmail.com>
2025-06-10 17:45:52 +0530

    fix: removed the title field as required from model

--- company/backend/src/models/Job.ts
+++ company/backend/src/models/Job.ts
@@ -2,14 +2,12 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IJob extends Document {
   jobId: string;
-  title: string;
   [key: string]: any;
 }
 
 const JobSchema: Schema = new Schema(
   {
-    jobId: { type: String, required: true, unique: true },
-    title: { type: String, required: true },
+    jobId: { type: String, required: true, unique: true }
   },
   { strict: false }
 );
